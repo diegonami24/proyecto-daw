2c602c0a6f8e5d0e871311ec94cd828d3ce45669
diegonami24 <armandortega92@hotmail.com>
2025-03-19 23:03:44 -0500

    avance 5

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-//scroll up
+document.getElementById("button-up").addEventListener("click", scrollUp);
 
-document.getElementById("button-up").addEventListener("click", scrollup);
+function scrollUp(){
 
-function scrollup(){
-    
-    var currentScroll = document.documentElement.scrollTop;
-    
-    if (currentScroll>0){
-        window.requestAnimationFrame(scrollup);
-        window.scrollTo (0,currentScroll -(currentScroll/10));
-    } 
+    var currentscroll = document.documentElement.scrollTop;
 
+    if (currentscroll > 0){
+        window.requestAnimationFrame(scrollUp);
+
+        window.scrollTo (0,currentscroll - (currentscroll / 20));
+    }
 }
 
-buttonUp = document.getElementById(button-up);
+buttonUp = document.getElementById("button-up");
 
-window.onscroll = function(){
+window.onscroll= function(){
 
     var scroll = document.documentElement.scrollTop;
 
-    if (scroll > 100){
-       buttonUp.style.transform = "scale(1)";
+    if (scroll > 500){
+        buttonUp.style.transform = "scale(1)";
+    }else if(scroll<500){
+        buttonUp.style.transform = "scale(0)";
     }
 }
